210c365786fd6be0a8dae6e0ba88c7f5850e8de0
Your Name <richardbriansantos@Richards-MacBook-Pro.local>
2021-02-22 20:33:59 -0800

    bootstrap grid system

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var tasks = {};
 
-var createTask = function(taskText, taskDate, taskList) {
+var createTask = function (taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
   var taskSpan = $("<span>")
@@ -18,7 +18,7 @@ var createTask = function(taskText, taskDate, taskList) {
   $("#list-" + taskList).append(taskLi);
 };
 
-var loadTasks = function() {
+var loadTasks = function () {
   tasks = JSON.parse(localStorage.getItem("tasks"));
 
   // if nothing in localStorage, create a new object to track all task status arrays
@@ -32,15 +32,15 @@ var loadTasks = function() {
   }
 
   // loop over object properties
-  $.each(tasks, function(list, arr) {
+  $.each(tasks, function (list, arr) {
     // then loop over sub-array
-    arr.forEach(function(task) {
+    arr.forEach(function (task) {
       createTask(task.text, task.date, list);
     });
   });
 };
 
-var saveTasks = function() {
+var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
@@ -48,19 +48,19 @@ var saveTasks = function() {
 
 
 // modal was triggered
-$("#task-form-modal").on("show.bs.modal", function() {
+$("#task-form-modal").on("show.bs.modal", function () {
   // clear values
   $("#modalTaskDescription, #modalDueDate").val("");
 });
 
 // modal is fully visible
-$("#task-form-modal").on("shown.bs.modal", function() {
+$("#task-form-modal").on("shown.bs.modal", function () {
   // highlight textarea
   $("#modalTaskDescription").trigger("focus");
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-primary").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -84,7 +84,7 @@ $("#task-form-modal .btn-primary").click(function() {
 
 
 // added event and callback function - 5.1.6
-$(".list-group").on("click", "p", function() {
+$(".list-group").on("click", "p", function () {
   var text = $(this)
     .text()
     .trim();
@@ -100,7 +100,7 @@ $(".list-group").on("click", "p", function() {
 });
 
 // add blur event - 5.1.6
-$(".list-group").on("blur", "textarea", function() {
+$(".list-group").on("blur", "textarea", function () {
   // get the textarea's current value/text
   var text = $(this)
     .val()
@@ -111,7 +111,7 @@ $(".list-group").on("blur", "textarea", function() {
     .closest(".list-group")
     .attr("id")
     .replace("list-", "");
-  
+
   // get the task's position in the list of other li elements
   var index = $(this)
     .closest(".list-group-item")
@@ -130,7 +130,7 @@ $(".list-group").on("blur", "textarea", function() {
 });
 
 // added event and function for Due Dates - 5.1.7
-$(".list-group").on("click", "span", function() {
+$(".list-group").on("click", "span", function () {
   // get current text
   var date = $(this)
     .text()
@@ -151,7 +151,7 @@ $(".list-group").on("click", "span", function() {
 
 // convert back when click outside - 5.1.7
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("blur", "input[type='text']", function () {
   // get current text
   var date = $(this)
     .val()
@@ -162,7 +162,7 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .closest(".list-group")
     .attr("id")
     .replace("list-", "");
-  
+
   // get the task's position in the list of other li elements
   var index = $(this)
     .closest(".list-group-item")
@@ -182,7 +182,7 @@ $(".list-group").on("blur", "input[type='text']", function() {
 });
 
 // remove all tasks
-$("#remove-tasks").on("click", function() {
+$("#remove-tasks").on("click", function () {
   for (var key in tasks) {
     tasks[key].length = 0;
     $("#list-" + key).empty();
