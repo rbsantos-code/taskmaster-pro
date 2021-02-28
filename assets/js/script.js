4a1aac31fad3506ab7c080a9ead987235dbdfe02
Your Name <richardbriansantos@Richards-MacBook-Pro.local>
2021-02-28 15:14:17 -0800

    more touch ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -238,16 +238,21 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activate: function(event) {
-    console.log("deactivate", this);
+  activate: function(event, ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function (event, ui) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
-  update: function(event) {
+  update: function() {
     // array to store the task data in
     var tempArr = [];
 
